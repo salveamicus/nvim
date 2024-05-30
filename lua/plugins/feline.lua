5377217e477061c2ad41a93e50e493fb770f51ae
James Osborne <jwosborne@knox.edu>
2024-05-30 17:36:19 -0500

    better look, mouse support buffer switch

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -3,7 +3,6 @@ return {
     opts = {},
     config = function(_, opts)
         require('feline').setup()
-        require('feline').winbar.setup()       -- to use winbar
         require('feline').statuscolumn.setup() -- to use statuscolumn
     end
 }
